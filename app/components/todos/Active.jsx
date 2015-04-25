904c7e9577efb4a36ceb52b4747b3670ad53d118
natew <natewienert@gmail.com>
2015-04-25 12:15:23 -0700

    right styles

--- app/components/todos/Active.jsx
+++ app/components/todos/Active.jsx
@@ -3,7 +3,7 @@ import { React } from 'reapp-kit';
 export default class {
   render() {
     return (
-      <ul class="todo-items">
+      <ul id="todo-list">
         {this.props.todoItems}
       </ul>
     )
